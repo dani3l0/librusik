81283680e8a0da1410e8c2793228544fd4519753
Daniel <daniel.jank@protonmail.com>
2023-01-10 10:18:10 +0100

    dump_full_api: add option to dump only selected API path

--- tools/dump_full_api.py
+++ tools/dump_full_api.py
@@ -2,7 +2,7 @@ import sys
 import os
 import asyncio
 import json
-import getpass
+from getpass import getpass as passw
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -13,11 +13,20 @@ from librus import Librus
 
 async def main():
 	print("---------------- Librus API Dumper ----------------")
-	_user = input("Librus Synergia login:     ")
-	_pass = getpass.getpass("Librus Synergia password:  ")
+
+	_user =					input("Librus Synergia login:        ")
+	_pass =					passw("Librus Synergia password:     ")
+	_full_dump =			input("Full API dump? [Y/n]:         ")
+	
+	_full_dump = _full_dump.strip().lower() not in ["n", "no", "false"]
+
+	if _full_dump:
+		print(f"Dumping full API...")
+	else:
+		_api_path =			input("Enter API path to be dumped:  ").strip()
+		print(f"Dumping only '{_api_path}' from API...")
 
 	librus = Librus(None)
-
 	if not await librus.mktoken(_user, _pass):
 		print("Wrong credentials.")
 		sys.exit(1)
@@ -26,12 +35,16 @@ async def main():
 	res_big = res.copy()
 	i = 0
 
-	for x in res:
-		i += 1
-		l = len(res)
-		print(f"[{str(round(i * 100 / l)).rjust(3)}%  {str(i).rjust(len(str(l)))}/{l}]  Requesting '{x}'")
-		item = await librus.get_data(x)
-		res_big[x] = item
+	if _full_dump:
+		for x in res:
+			i += 1
+			l = len(res)
+			print(f"[{str(round(i * 100 / l)).rjust(3)}%  {str(i).rjust(len(str(l)))}/{l}]  Requesting '{x}'")
+			item = await librus.get_data(x)
+			res_big[x] = item
+	else:
+		data = await librus.get_data(_api_path)
+		res_big[_api_path] = data
 
 	print("Saving data to JSON...")
 	result = open("api_dump.json", "w")
